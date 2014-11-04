494793926a9afad8b3d41cc33917d652232be94c
Stephen Caldwell <steve@campusexplorer.com>
2014-11-04 10:02:30 -0800

    I had to do this to get 'lein uberjar' to work

--- project.clj
+++ project.clj
@@ -2,9 +2,8 @@
   :description "Hadoop utilities for MrJob that must run in the JVM"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.2"]
+                 [org.apache.hadoop/hadoop-streaming "0.20.2"]
                  [org.clojure/data.csv "0.1.2"]]
   :license {:name "Apache License, Version 2.0 "
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]]}}
   :aot :all)
